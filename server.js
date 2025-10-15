27c23ddb15ca08cee04c4d879cb157aa01682605
Aaron <aaron.gerkens@proton.me>
2025-10-15 16:34:52 +0200

    made some changes idk

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
+    res.redirect(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/github', (req, res) => {
+    res.redirect("https://github.com/cns-studios");
+});
+
+app.get('/docs', (req, res) => {
+    res.redirect(path.join(__dirname, 'public', 'docs.html'));
+});
+
+app.get('/login', (req, res) => {
     res.redirect(path.join(__dirname, 'public', 'login.html'));
 });
 
